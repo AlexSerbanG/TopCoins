5d0c53d8b65fbbe783fb7933bc9daabb933088ac
alex.serbang@gmail.com <alex.serbang@gmail.com>
2021-05-28 10:27:59 +0300

    extracted separate component for tables

--- src/components/MarketOverview/MarketOverview.jsx
+++ src/components/MarketOverview/MarketOverview.jsx
@@ -1,53 +1,7 @@
 import * as React from 'react';
-import { useTable } from 'react-table';
-import MaUTable from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
 import { CircularProgress } from '@material-ui/core';
 import { useLatestCoins } from '../../hooks/useLatestCoins';
-
-function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
-    columns,
-    data,
-  })
-
-  // Render the UI for your table
-  return (
-    <MaUTable {...getTableProps()}>
-      <TableHead>
-        {headerGroups.map(headerGroup => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <TableCell {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <TableCell {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </TableCell>
-                )
-              })}
-            </TableRow>
-          )
-        })}
-      </TableBody>
-    </MaUTable>
-  )
-}
+import { Table } from '../common/MauTable';
 
 export const MarketOverview = () => {
   const { data, isLoading, isError } = useLatestCoins();
